c30ea0353d471e23a07db8ab18d76b43a2da31ec
Stefan Ziegler <edi.gonzales@gmail.com>
2019-08-06 12:02:00 +0200

    fix

--- src/main/java/ch/so/agi/oereb/OerebIconizer.java
+++ src/main/java/ch/so/agi/oereb/OerebIconizer.java
@@ -38,20 +38,7 @@ public class OerebIconizer {
         return legendEntries;
     }
     
-    /**
-     * Gets all the symbols and the according type code from a QGIS 3 wms server.
-     * 
-     * @param configFileName GetStyles request url (= SLD file).
-     * @param legendGraphicUrl GetLegendGraphic request url with the vendor specific parameters for single symbol support. 
-     * The RULE parameter is added dynamically. The LAYER parameter must be included.
-     * @throws Exception
-     */
-    public List<LegendEntry> getSymbolsQgis3Substring(String configFileName, String legendGraphicUrl) throws Exception {
-        SymbolTypeCodeBuilder styleConfigBuilder = new Qgis3SubstringSymbolTypeCodeBuilder(configFileName, legendGraphicUrl);
-        List<LegendEntry> legendEntries = styleConfigBuilder.build();
-        return legendEntries;
-    }    
-    
+      
     /**
      * Saves symbols to disk. The type code is the file name.
      * 
